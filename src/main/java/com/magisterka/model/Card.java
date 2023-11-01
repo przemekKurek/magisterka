f74d61139465e1a844707ff6908a5d06268f4cfe
przemyslaw <przemyslaw.kurek@bluesoft.com>
2023-11-01 22:25:04 +0100

    model

---
+++ src/main/java/com/magisterka/model/Card.java
@@ -0,0 +1,10 @@
+package com.magisterka.model;
+
+
+import lombok.Data;
+
+@Data
+public class Card {
+    private Colour colour;
+    private Figure figure;
+}
---
+++ src/main/java/com/magisterka/model/Figure.java
@@ -0,0 +1,17 @@
+package com.magisterka.model;
+
+public enum Figure {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+}
