a378a0dedfbd6ee15d068b3c8a0dcfcd5e1f70a5
przemyslaw <przemyslaw.kurek@bluesoft.com>
2023-11-01 22:05:00 +0100

    pierwszy commit

---
+++ src/main/java/com/magisterka/App.java
@@ -0,0 +1,13 @@
+package com.magisterka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "tutaj będzie apka" );
+    }
+}
