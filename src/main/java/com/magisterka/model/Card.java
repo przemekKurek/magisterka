96cd4f011575597a29b3514ee78d04d7d2a78bd4
przemyslaw <przemyslaw.kurek@bluesoft.com>
2023-11-04 12:46:28 +0100

    model

--- src/main/java/com/magisterka/model/Card.java
+++ src/main/java/com/magisterka/model/Card.java
@@ -1,10 +1,26 @@
 package com.magisterka.model;
 
-
 import lombok.Data;
 
 @Data
 public class Card {
-    private Colour colour;
-    private Figure figure;
+    private int cardNumber;
+
+    public Card(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getSuit() {
+        int suitValue = cardNumber / 13;
+        // You can define an array to map numeric suit values to actual suits.
+        String[] suits = {"Spades", "Clubs", "Diamonds", "Hearts"};
+        return suits[suitValue];
+    }
+
+    public String getRank() {
+        int rankValue = cardNumber % 13;
+        // You can define an array to map numeric rank values to actual ranks.
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        return ranks[rankValue];
+    }
 }
