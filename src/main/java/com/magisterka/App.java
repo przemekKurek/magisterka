008e6559601ca48a1ddc7cda7da40fe82b79dfcd
przemyslaw <przemyslaw.kurek@bluesoft.com>
2023-11-06 18:56:40 +0100

    first version of game

--- src/main/java/com/magisterka/App.java
+++ src/main/java/com/magisterka/App.java
@@ -1,17 +1,10 @@
 package com.magisterka;
 
-import com.magisterka.service.GameService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    private static GameService gameService = new GameService();
-
-    public App(GameService gameService) {
-        this.gameService = gameService;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
--- src/main/java/com/magisterka/web/GameController.java
+++ src/main/java/com/magisterka/web/GameController.java
@@ -1,20 +1,19 @@
 package com.magisterka.web;
 
 import com.magisterka.service.GameService;
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Data
-@RequestMapping("/")
+@RequestMapping("/game")
+@AllArgsConstructor
 public class GameController {
 
     private final GameService gameService;
 
-
-    @GetMapping(value = "/game")
+    @GetMapping(value = "/two")
     public int game()
     {
       return gameService.game(2);
